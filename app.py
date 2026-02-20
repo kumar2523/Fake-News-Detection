260f8b1b6b704adc0294fe8fbfba8f7a9fa25977
Nagabhyru kumar raja <nagabhyrukumarraja@gmail.com>
2026-02-20 22:46:03 +0530

    Updated app without pickle

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
-import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+# Load dataset
+df_fake = pd.read_csv("Fake.csv")
+df_true = pd.read_csv("True.csv")
 
-# Load saved model
-model = pickle.load(open("model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+df_fake["label"] = 0
+df_true["label"] = 1
+
+df = pd.concat([df_fake, df_true])
+df = df.sample(frac=1)
+
+# Prepare data
+X = df["text"]
+y = df["label"]
+
+# Vectorization
+vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+X = vectorizer.fit_transform(X)
+
+# Train model
+model = LogisticRegression()
+model.fit(X, y)
 
 st.title("📰 Fake News Detector")
 
