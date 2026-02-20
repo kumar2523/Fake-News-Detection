e3d277c6c23118d4ed57ae60374d18a3ba72283a
Nagabhyru kumar raja <nagabhyrukumarraja@gmail.com>
2026-02-20 22:55:46 +0530

    final fix for streamlit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+
 # Load dataset
 df_fake = pd.read_csv("Fake.csv")
 df_true = pd.read_csv("True.csv")
@@ -9,8 +10,8 @@ df_true = pd.read_csv("True.csv")
 df_fake["label"] = 0
 df_true["label"] = 1
 
-df = pd.concat([df_fake, df_true])
-df = df.sample(frac=1)
+# ✅ Reduced dataset size (important for Streamlit Cloud)
+df = pd.concat([df_fake, df_true]).sample(n=3000)
 
 # Prepare data
 X = df["text"]
@@ -21,7 +22,7 @@ vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 X = vectorizer.fit_transform(X)
 
 # Train model
-model = LogisticRegression()
+model = LogisticRegression(max_iter=100)
 model.fit(X, y)
 
 st.title("📰 Fake News Detector")
